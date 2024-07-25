529cce79f088ae81ebf24f3b857f6a2634842929
Ícaro <icarovisk239@gmail.com>
2024-07-24 23:39:03 -0300

    fixed a live_reload bug

--- conduit/mix.exs
+++ conduit/mix.exs
@@ -58,7 +58,8 @@ defmodule Conduit.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:guardian, "~> 2.0"}
+      {:guardian, "~> 2.0"},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
